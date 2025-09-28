aeace30dce32125b31139e4ada1640eca5581c0c
OConnell <killian.oconnell@sap.com>
2025-09-28 14:07:34 +0100

    Add previous lab Python and Bash scripts

---
+++ hello.py
@@ -0,0 +1,20 @@
+# hello.py
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return (
+        '<p>Hello, World! I am a Flask app!</p>'
+        '<p><a href="/about">About this app</a></p>'
+    )
+
+@app.route("/about")
+def about():
+    return (
+        '<p>This application is running on the Flask web framework.</p>'
+        '<p>Learn more about Flask here: '
+        '<a href="https://flask.palletsprojects.com/" target="_blank">Flask Documentation</a></p>'
+        '<p><a href="/">Back to Home</a></p>'
+    )
