baf1a2de5d057720f508d3091e5b7072ddc3600f
OConnell <killian.oconnell@sap.com>
2025-09-28 17:31:58 +0100

    Change homepage greeting to 'This is another string' on main

--- hello.py
+++ hello.py
@@ -8,6 +8,8 @@ def home():
     return (
         '<p>Hello, World! I am a Flask app!</p>'
         '<p><a href="/about">About this app</a></p>'
+        '<p>This is another string!</p>'
+
     )
 
 @app.route("/about")
